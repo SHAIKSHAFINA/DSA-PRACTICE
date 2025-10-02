6724c14bfb26fe6d843013a7e230e88b6d519e94
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2025-10-02 17:19:57 +0530

    Time: 1 ms (99.75%), Space: 53.6 MB (14.92%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n=nums.length;
-        int c=0,can=nums[0],l=0;
-        for(int i=1;i<n;i++){
-           if(can==nums[l]){
-                c++;
+        int c=0,can=0;
+        for(int i=0;i<n;i++){
+            if(c==0){
+                can=nums[i];
+            }
+           if(can==nums[i]){
+            c++;
            }
            else{
             c--;
            }
-            if(c==0){
-                can=nums[i];
-            }
-            l++;
         }
         return can;
     }
