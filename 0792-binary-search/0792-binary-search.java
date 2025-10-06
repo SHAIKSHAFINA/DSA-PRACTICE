16117edd3bd15f89985ccf0fdbee44310729a763
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2025-10-06 15:10:58 +0530

    Time: 0 ms (100%), Space: 45.8 MB (47.18%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -7,17 +7,17 @@ class Solution {
 
         int l=0,r=n-1;
         while(l<=r){
-            int mid=(l+r)/2;
+            int mid=l+(r-l)/2;
 
             if(nums[mid]==target){
                 return mid;
             }
-            if(nums[l]<target){
-                l++;
+            if(nums[mid]<target){
+                l=mid+1;
                
             }
-            else if(nums[r]>=target){
-                r--;
+            else{
+                r=mid-1;
             }
         }
         return -1;
