2c49dea8f64f9b98cb8b2131686ac3c1192811fc
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2025-10-13 23:54:17 +0530

    Time: 1 ms (85.21%), Space: 40.8 MB (71.28%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        int l=1,r=x/2, ans=0;
+
+        if(x==0) return 0;
+        if(x==1) return 1;
+        while(l<=r){
+            long mid=l+(r-l)/2;
+            long sq=mid*mid;
+            if(sq==x){
+                return (int)mid;
+            }
+            else if(sq<x){
+                ans=(int)mid;
+                l=(int)mid+1;
+            }
+            else{
+                r=(int)mid-1;
+            }
+        }
+        return ans;
     }
 }
