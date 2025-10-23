39f788eb1634ac659058d9ec9264f6ed7db591be
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2025-10-23 20:08:04 +0530

    Time: 0 ms (100%), Space: 41.3 MB (70.53%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,20 +1,21 @@
 class Solution {
     public int splitArray(int[] arr, int k) {
         int n=arr.length;
+
         if(k>n) return -1;
-        
-        int sum=0,maxi=Integer.MIN_VALUE;
-        
-        for(int s:arr){
-            maxi=Math.max(maxi,s);
-            sum+=s;
+
+        int s=0,max=Integer.MIN_VALUE;
+
+        for(int x:arr){
+            s+=x;
+            max=Math.max(max,x);
         }
-        
-        int l=maxi,r=sum,ans=0;
-        
+
+        int l=max,r=s;
+
         while(l<=r){
+
             int mid=l+(r-l)/2;
-            
             if(possible(arr,mid,k)==true){
                 r=mid-1;
             }
@@ -23,27 +24,25 @@ class Solution {
             }
         }
         return l;
-        
-        
     }
-    
-    boolean possible(int[]arr,int dist,int k){
+
+    boolean possible(int arr[],int dist,int k){
         int n=arr.length;
-        
+
         int sum=0,c=1;
-        
+
         for(int i=0;i<n;i++){
-            if(sum+arr[i]>dist){
+            if(arr[i]+sum>dist){
                 c++;
                 sum=arr[i];
             }
             else{
                 sum+=arr[i];
             }
-            
+
             if(c>k) return false;
         }
+
         return true;
-        
     }
 }
