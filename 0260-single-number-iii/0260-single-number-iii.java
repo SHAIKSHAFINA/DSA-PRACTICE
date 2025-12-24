abc61abaa36b88fda323bdfee05eaba51d32c2db
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2025-12-24 20:42:36 +0530

    Time: 4 ms (43.19%), Space: 48 MB (49.86%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -2,18 +2,37 @@ class Solution {
     public int[] singleNumber(int[] nums) {
         int n=nums.length;
         int[] arr=new int[2];
-        HashMap<Integer,Integer> mp=new HashMap<>();
+        ArrayList<Integer>a=new ArrayList<>();
+        ArrayList<Integer>b=new ArrayList<>();
+
+
+        int ans=0;
         for(int i=0;i<n;i++){
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+            ans^=nums[i];
         }
 
-         int idx=0;
-        for(Map.Entry<Integer,Integer> entry:mp.entrySet()){
-            if(entry.getValue()==1){
-                arr[idx]=entry.getKey();
-                idx++;
+        int mask=ans & (-ans);
+        for(int i=0;i<n;i++){
+            if((nums[i] & mask)!=0){
+                a.add(nums[i]);
+            }
+            else{
+                b.add(nums[i]);
             }
         }
+        int a1=0;
+        for(int i=0;i<a.size();i++){
+            a1^=a.get(i);
+        }
+        int a2=0;
+        for(int i=0;i<b.size();i++){
+            a2^=b.get(i);
+        }
+
+        arr[0]=a1;
+        arr[1]=a2;
         return arr;
+        
+
     }
 }
