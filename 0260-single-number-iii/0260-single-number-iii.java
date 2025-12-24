6df263142a0102eb59fac0ca9516b88cea5fe586
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2025-12-24 20:46:59 +0530

    Time: 1 ms (99.82%), Space: 48 MB (32.09%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -2,33 +2,21 @@ class Solution {
     public int[] singleNumber(int[] nums) {
         int n=nums.length;
         int[] arr=new int[2];
-        ArrayList<Integer>a=new ArrayList<>();
-        ArrayList<Integer>b=new ArrayList<>();
-
 
         int ans=0;
         for(int i=0;i<n;i++){
             ans^=nums[i];
         }
 
-        int mask=ans & (-ans);
+        int mask=ans & (-ans) , a1=0 ,a2=0;
         for(int i=0;i<n;i++){
             if((nums[i] & mask)!=0){
-                a.add(nums[i]);
+                a1^=nums[i];
             }
             else{
-                b.add(nums[i]);
+                a2^=nums[i];
             }
         }
-        int a1=0;
-        for(int i=0;i<a.size();i++){
-            a1^=a.get(i);
-        }
-        int a2=0;
-        for(int i=0;i<b.size();i++){
-            a2^=b.get(i);
-        }
-
         arr[0]=a1;
         arr[1]=a2;
         return arr;
