fe119d7faa5e06d4061b5f2fec70db06753f4290
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-01-06 22:31:40 +0530

    Time: 0 ms (100%), Space: 48.3 MB (21.85%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,19 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-        double res=1;
-        long exp=n;
-        
-        if(n<0){
-           exp=Math.abs((long)n);
-           x=1/x;
-        }
-        while(exp!=0){
+       double res=1;
+       long exp=n;
+
+       if(n<0){
+        exp=Math.abs((long)n);
+        x=1/x;
+       }
+
+       while(exp!=0){
             if(exp%2==1){
-                 res*=x;
+                res*=x;
             }
             x*=x;
             exp/=2;
-        }
-        return res;
+       }
+
+       return res;
     }
 }
