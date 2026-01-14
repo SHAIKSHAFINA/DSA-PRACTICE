3b73c0df4c3317518460ded30884413724ebedbf
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-01-14 22:22:05 +0530

    Time: 3 ms (92.46%), Space: 45.4 MB (39.09%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,52 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n=nums1.length;
-        int m=nums2.length;
+        int[] ans = new int[nums1.length];
 
-        int arr[]=new int[n];
+        HashMap<Integer, Integer> nge = new HashMap<>();
+        Deque<Integer> st = new ArrayDeque<>();
 
-        HashMap<Integer,Integer> mp=new HashMap<>();
-        ArrayList<Integer> sc=nle(nums2);
-
-        for(int i=0;i<m;i++){
-            mp.put(nums2[i],sc.get(i));
-        }
-        for(int i=0;i<n;i++){
-
-            if(mp.containsKey(nums1[i])){
-                arr[i]=mp.get(nums1[i]);
+        for (int x : nums2) {
+            while (!st.isEmpty() && st.peek() < x) {
+                nge.put(st.pop(), x);
             }
-            else{
-                arr[i]=-1;
-            }
-            
+            st.push(x);
         }
 
-        return arr;
-
-         
-    }
-    ArrayList<Integer>nle(int[]arr){
-        int n=arr.length;
-        ArrayList<Integer> sc=new ArrayList<>();
-        Stack<Integer> st=new Stack<>();
-        
-        for(int i=n-1;i>=0;i--){
-        
-            while(!st.isEmpty() && arr[i]>=st.peek()){
-                    st.pop();
-            }
-                
-            if(st.isEmpty()){
-                sc.add(-1);
-            }
-            else{
-                sc.add(st.peek());
-            }
-            st.push(arr[i]);
+        while (!st.isEmpty()) {
+            nge.put(st.pop(), -1);
         }
-        Collections.reverse(sc);
-        
-        return sc;
+
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = nge.get(nums1[i]); 
+        }
+
+        return ans;
     }
 }
