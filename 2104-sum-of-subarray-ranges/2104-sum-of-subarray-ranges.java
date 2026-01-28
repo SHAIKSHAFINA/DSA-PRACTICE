bcb892c724e31b736550dbf74dc8249a3a2e093a
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-01-28 23:23:16 +0530

    Time: 49 ms (5.65%), Space: 46.5 MB (59.2%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,20 +1,111 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
         int n=nums.length;
-        // int min=Integer.MAX_VALUE;
-        // int max=Integer.MIN_VALUE;
-        long sum=0;
+        long total=0;
+        ArrayList<Integer>nse=findNse(nums);
+        ArrayList<Integer>pse=findPse(nums);
+        ArrayList<Integer>nge=findNge(nums);
+        ArrayList<Integer>pge=findPge(nums);
+
 
         for(int i=0;i<n;i++){
-           int min=nums[i];
-            int max=nums[i];
-            for(int j=i;j<n;j++){
-                min=Math.min(min,nums[j]);
-                max=Math.max(max,nums[j]);
-                 sum+=(max-min);
-            }
-           
+            int left1=i-pse.get(i);
+            int right1=nse.get(i)-i;
+            int left2=i-pge.get(i);
+            int right2=nge.get(i)-i;
+            long freq1= left1*right1*1L;
+            long freq2= left2*right2*1L;
+            long val= (freq2* nums[i])-(freq1* nums[i]);
+            total= (total+val);
         }
-        return sum;
+        return total;
+    }
+    ArrayList<Integer> findNse(int [] nums){
+        int n=nums.length;
+        ArrayList<Integer> sc=new ArrayList<>();
+        Stack<Integer> st=new Stack<>();
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+               sc.add(n);
+            }
+            else{
+                sc.add(st.peek());
+            }
+            st.push(i);
+        }
+        Collections.reverse(sc);
+        return sc;
+    }
+
+    ArrayList<Integer> findPse(int[] arr){
+        int n=arr.length;
+        Stack<Integer> st=new Stack<>();
+        ArrayList<Integer> sc=new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && arr[i]<arr[st.peek()]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+                sc.add(-1);
+            }
+            else{
+                sc.add(st.peek());
+            }
+            st.push(i);
+        }
+       
+        return sc;
+    }
+
+    ArrayList<Integer> findNge(int[] arr){
+        int n=arr.length;
+        Stack<Integer> st=new Stack<>();
+        ArrayList<Integer> sc=new ArrayList<>();
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && arr[i]>=arr[st.peek()]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+                sc.add(n);
+            }
+            else{
+                sc.add(st.peek());
+            }
+            st.push(i);
+        }
+        Collections.reverse(sc);
+       
+        return sc;
+    }
+
+    ArrayList<Integer> findPge(int[] arr){
+        int n=arr.length;
+        Stack<Integer> st=new Stack<>();
+        ArrayList<Integer> sc=new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && arr[i]>arr[st.peek()]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+                sc.add(-1);
+            }
+            else{
+                sc.add(st.peek());
+            }
+            st.push(i);
+        }
+       
+        return sc;
     }
 }
