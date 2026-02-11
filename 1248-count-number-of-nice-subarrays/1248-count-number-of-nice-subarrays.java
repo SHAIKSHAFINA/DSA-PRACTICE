8e6f37bb50f5002ed1dd75e0c3df08d036a9f3f6
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-02-11 05:38:04 +0530

    Time: 10 ms (87.93%), Space: 59.5 MB (65.44%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,49 +1,25 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         int n=nums.length;
-        return subArray1(nums,k)-subArray2(nums,k-1);
+        return subArray(nums,k)-subArray(nums,k-1);
     }
 
-    int subArray1(int[]nums , int k){
+    int subArray(int[]nums , int k){
         int l=0,r=0,n=nums.length;
         int ps=0,c=0;
         if(k<0) return 0;
 
         while(r<n){
-            if(nums[r]%2==0){
-                nums[r]=0;
-            }
-            else{
-                nums[r]=1;
-            }
+            // if(nums[r]%2==0){
+            //     nums[r]=0;
+            // }
+            // else{
+            //     nums[r]=1;
+            // }
 
-            ps+=nums[r];
+            ps+=nums[r]%2;
             while(ps>k){
-                ps-=nums[l];
-                l++;
-            }
-            c+=(r-l+1);
-            r++;
-        }
-        return c;
-
-    }
-    int subArray2(int[]nums , int k){
-        int l=0,r=0,n=nums.length;
-        int ps=0,c=0;
-        if(k<0) return 0;
-
-        while(r<n){
-            if(nums[r]%2==0){
-                nums[r]=0;
-            }
-            else{
-                nums[r]=1;
-            }
-
-            ps+=nums[r];
-            while(ps>k){
-                ps-=nums[l];
+                ps-=nums[l]%2;
                 l++;
             }
             c+=(r-l+1);
