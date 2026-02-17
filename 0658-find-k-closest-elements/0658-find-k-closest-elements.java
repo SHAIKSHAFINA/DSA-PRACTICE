db807115c7d494becea43575b8302d06e1ca3fa1
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-02-17 18:33:16 +0530

    Time: 4 ms (98.18%), Space: 47.4 MB (97.56%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,31 +1,24 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-       int n=arr.length;
-       PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->{
-            if(a[0]!=b[0]){
-                return b[0]-a[0];
+        int n=arr.length;
+        List<Integer>sc=new ArrayList<>();
+        int l=0,r=n-1;
+
+        while(r-l+1 > k){
+            int st=Math.abs(arr[l]-x);
+            int end=Math.abs(arr[r]-x);
+
+            if(st > end){
+                l++;
             }
             else{
-                return b[1]-a[1];
+                r--;
             }
-       });
+        }
 
-       for(int i=0;i<n;i++){
-            int dist=Math.abs(arr[i]-x);
-            int val=arr[i];
-            pq.add(new int[]{dist,val});
-            if(pq.size()>k){
-                pq.poll();
-            }
-       } 
-
-       
-       List<Integer> mp=new ArrayList<>();
-
-       for(int i=0;i<k;i++){
-        mp.add(pq.poll()[1]);
-       }
-       Collections.sort(mp);
-       return mp;
+        for(int i=l;i<=r;i++){
+            sc.add(arr[i]);
+        }
+        return sc;
     }
 }
