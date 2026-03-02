1fb31812c14e4248867e6e1533af728be4a71bee
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-03-02 22:37:11 +0530

    Time: 38 ms (15.16%), Space: 48 MB (89.01%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,21 +1,21 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         int x=tasks.length;
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
         HashMap<Character,Integer> mp=new HashMap<>();
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
 
         for(int i=0;i<x;i++){
             mp.put(tasks[i],mp.getOrDefault(tasks[i],0)+1);
         }
 
         for(int num:mp.values()){
-            pq.offer(num);
+            pq.add(num);
         }
-
         int time=0;
 
         while(!pq.isEmpty()){
             ArrayList<Integer> sc=new ArrayList<>();
+
             for(int i=1;i<=n+1;i++){
                 if(!pq.isEmpty()){
                     int freq=pq.poll();
@@ -24,12 +24,11 @@ class Solution {
                 }
             }
 
-        
-        for(int f:sc){
-            if(f>0){
-                pq.add(f);
+            for(int y:sc){
+                if(y>0){
+                    pq.add(y);
+                }
             }
-        }
 
             if(pq.isEmpty()){
                 time+=sc.size();
@@ -37,11 +36,7 @@ class Solution {
             else{
                 time+=n+1;
             }
-        
         }
-        
         return time;
-
-        
     }
 }
