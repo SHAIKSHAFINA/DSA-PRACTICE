864a0de78b4196469a8f9ad275894a458cf49e77
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-03-06 09:03:50 +0530

    Time: 154 ms (77.34%), Space: 149.3 MB (96.09%) - LeetHub

--- 3478-choose-k-elements-with-maximum-sum/3478-choose-k-elements-with-maximum-sum.java
+++ 3478-choose-k-elements-with-maximum-sum/3478-choose-k-elements-with-maximum-sum.java
@@ -12,24 +12,24 @@ class Solution {
     public long[] findMaxSum(int[] nums1, int[] nums2, int k) {
         int n=nums1.length;
 
-        ArrayList<Pair> sc=new ArrayList<>();
+        Pair[] arr=new Pair[n];
         for(int i=0;i<n;i++){
-            sc.add(new Pair(nums1[i],i,nums2[i]));
+            arr[i]=(new Pair(nums1[i],i,nums2[i]));
         }
-        Collections.sort(sc,(a,b)->a.val1-b.val1);
+        Arrays.sort(arr,(a,b)->a.val1-b.val1);
         long[] f=new long[n];
         PriorityQueue<Integer> pq=new PriorityQueue<>();
         long sum=0;
 
         for(int i=0;i<n;i++){
-            Pair p=sc.get(i);
+            Pair p=arr[i];
            int x=p.val1;
            int idx=p.index;
             int j=p.val2;
 
 
-            if(i>0 && sc.get(i).val1 ==sc.get(i-1).val1){
-                f[idx]=f[sc.get(i-1).index];
+            if(i>0 && arr[i].val1 ==arr[i-1].val1){
+                f[idx]=f[arr[i-1].index];
             }
             else{
                 f[idx]=sum;
