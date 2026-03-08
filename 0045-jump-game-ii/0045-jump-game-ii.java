31f623e0270eee864bd63493dbb0c6c50606f9f7
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-03-08 22:05:43 +0530

    Time: 1 ms (99.61%), Space: 47.2 MB (74.03%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n=nums.length;
+        int l=0,r=0,c=0;
+
+        while(r<n-1){
+            int farthest=0;
+            for(int i=l;i<=r;i++){
+                farthest=Math.max(farthest,i+nums[i]);
+            }
+            c++;
+            l=r+1;
+            r=farthest;
+        }
+        return c;
+    }
+}
