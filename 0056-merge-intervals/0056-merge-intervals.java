d4e1333726260655e39b1eaf17acd402b30e6aa6
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-03-11 22:18:04 +0530

    Time: 8 ms (91.03%), Space: 49.3 MB (37.2%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,36 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int n=intervals.length;
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        ArrayList<int[]> sc=new ArrayList<>();
-        if(n==1 || n==0){
-            return intervals;
-        }
-        //sc.add(intervals[0][0]);
-        int currStart=intervals[0][0];
-        int currEnd=intervals[0][1];
 
-        for(int i=1;i<n;i++){
-            int[] a=new int[2];
-            if(intervals[i][0] <= currEnd){
-                currEnd=Math.max(currEnd,intervals[i][1]);
-            }
-            else{
-                sc.add(new int[]{currStart,currEnd});
-               currStart=intervals[i][0];
-               currEnd=intervals[i][1];
-            }
-        }
-        sc.add(new int[]{currStart,currEnd});
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
 
-        int[][] arr=new int[sc.size()][2];
+        int index = 0;
 
-        for(int i=0;i<sc.size();i++){
-            for(int j=0;j<2;j++){
-                arr[i][j]=sc.get(i)[j];
+        for(int i = 1; i < intervals.length; i++) {
+
+            if(intervals[i][0] <= intervals[index][1]) {
+                intervals[index][1] = Math.max(intervals[index][1], intervals[i][1]);
+            } 
+            else {
+                index++;
+                intervals[index] = intervals[i];
             }
         }
 
-        return arr;
+        return Arrays.copyOf(intervals, index + 1);
     }
 }
