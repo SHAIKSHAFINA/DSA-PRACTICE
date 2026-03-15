d58eade0d08c12d214465751856fe8c331d36c22
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-03-15 22:08:38 +0530

    Time: 1 ms (33.36%), Space: 43.9 MB (8.72%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,22 +1,18 @@
 class Solution {
     public boolean checkValidString(String s) {
         int n=s.length();
-        int dp[][]=new int[n][n+1];
-        
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
-        }
+        Boolean dp[][]=new Boolean[n][n+1];
         
         return recurr(s,0,0,dp);
     }
 
-    boolean recurr(String s,int i,int co , int[][]dp){
+    boolean recurr(String s,int i,int co , Boolean[][]dp){
         int n=s.length();
         if(i==n){
             return co==0;
         }
         if(co<0) return false;
-        if(dp[i][co] !=-1) return dp[i][co]==1;
+        if(dp[i][co] !=null) return dp[i][co];
         
         char c=s.charAt(i);
 
@@ -32,7 +28,7 @@ class Solution {
         else{
              result=recurr(s,i+1,co+1,dp) || recurr(s,i+1,co-1,dp) || recurr(s,i+1,co,dp);
         }
-        dp[i][co]=result?1:0;
+        dp[i][co]=result;
         return result;
 
         
