93ab480d7487aa2e10a230c1cf540a42c8d0bce6
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-03-17 17:30:47 +0530

    Time: 0 ms (100%), Space: 42.7 MB (65.27%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,37 +1,28 @@
 class Solution {
     public boolean checkValidString(String s) {
+        int min=0;
+        int max=0;
         int n=s.length();
-        Boolean dp[][]=new Boolean[n][n+1];
-        
-        return recurr(s,0,0,dp);
-    }
 
-    boolean recurr(String s,int i,int co , Boolean[][]dp){
-        int n=s.length();
-        if(i==n){
-            return co==0;
+        for(int i=0;i<n;i++){
+            char c=s.charAt(i);
+            
+            if(c=='('){
+                min++;
+                max++;
+            }
+            else if(c==')'){
+                min--;
+                max--;
+            }
+            else{
+               min=min-1;
+               max=max+1;
+            }
+            min=Math.max(0,min);
+            if(max <0) return false;
         }
-        if(co<0) return false;
-        if(dp[i][co] !=null) return dp[i][co];
-        
-        char c=s.charAt(i);
 
-        if(co<0) return false;
-        boolean result;
-        
-         if(c=='('){
-            result= recurr(s,i+1,co+1,dp);
-        }
-        else if(c==')'){
-            result= recurr(s,i+1,co-1,dp);
-        }
-        else{
-             result=recurr(s,i+1,co+1,dp) || recurr(s,i+1,co-1,dp) || recurr(s,i+1,co,dp);
-        }
-        dp[i][co]=result;
-        return result;
-
-        
-
+        return min==0;
     }
 }
