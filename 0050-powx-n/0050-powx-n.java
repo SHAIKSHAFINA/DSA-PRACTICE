5e765ae7bb7ef81e4da7008a0a1981f003ae3c1a
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-04-01 07:58:26 +0530

    Time: 0 ms (100%), Space: 47.5 MB (92.31%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,28 +1,28 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(x==0) return 0;
-        if(n==0) return 1;
+         if(x==0) return 0;
+         if(n==0) return 1;
         long N=n;
 
         if(N<0){
             x=1/x;
             N=-N;
         }
-        return (double)(power(x,N));
+
+        double res=1;
+
+        while(N>0){
+            if(N%2==1){
+                res*=x;
+            }
+            x=x*x;
+            N/=2;
+        }
+        return res;
+       
 
     }
 
-    double power(double x,long n){
-        if(x==0) return 0;
-        if(n==0) return 1;
-        double half=power(x,n/2);
-        double xpow=0;
-        if(n%2==0){
-            xpow=half* half;
-        }
-        else{
-            xpow=x* half * half;
-        }
-        return xpow;
-    }
+    
+    
 }
