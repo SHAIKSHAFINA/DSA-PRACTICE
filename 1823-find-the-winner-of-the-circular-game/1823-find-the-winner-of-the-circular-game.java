cc4daa172bd936e642115657bd4ecefc2b5542c8
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-04-08 22:23:48 +0530

    Time: 0 ms (100%), Space: 41.9 MB (97.48%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,10 +1,10 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        int ans=0;
+        return solve(n, k) + 1;
+    }
 
-        for(int i=1;i<=n;i++){
-            ans=(ans+k)% i;
-        }
-        return ans+1;
+    int solve(int n, int k){
+        if(n == 1) return 0;
+        return (solve(n - 1, k) + k) % n;
     }
 }
