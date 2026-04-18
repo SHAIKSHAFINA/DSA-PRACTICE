a48b2af67652b3f9441ef8ebe774776670fa3140
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-04-18 23:01:45 +0530

    Time: 7 ms (97.94%), Space: 65.3 MB (43.46%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,30 +1,28 @@
 class Solution {
-    public List<List<String>> partition(String s) {
+    public List<List<String>> partition(String sb) {
         List<List<String>> sc = new ArrayList<>();
         List<String> l = new ArrayList<>();
-        StringBuilder sb=new StringBuilder(s);
         solve(sb,l,sc,0);
         return sc;
     }
 
-    void solve(StringBuilder sb,List<String> l,List<List<String>> sc , int start){
+    void solve(String sb,List<String> l,List<List<String>> sc , int start){
         if(start==sb.length()){
             sc.add(new ArrayList<>(l));
             return;
         }
-        StringBuilder t=new StringBuilder();
         for(int i=start;i<sb.length();i++){
-            t.append(sb.charAt(i));
-            if(isPalindrome(t)){
-                l.add(t.toString());
+            String t=sb.substring(start,i+1);
+
+            if(isPalindrome(sb,start,i)){
+                l.add(t);
                 solve(sb,l,sc,i+1);
                 l.remove(l.size()-1);
             }
         }
     }
 
-    boolean isPalindrome(StringBuilder sb){
-        int l=0,r=sb.length()-1;
+    boolean isPalindrome(String sb,int l,int r){
 
         while(l<r){
             if(sb.charAt(l)!=sb.charAt(r)){
