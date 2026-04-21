101d6b70547426a4be801fb22e44901a865f149f
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-04-22 00:46:45 +0530

    Time: 1 ms (99.9%), Space: 46.6 MB (66.51%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,59 +1,51 @@
 class Solution {
-    List<List<String>>ans=new ArrayList<>();
+    List<List<String>> ans = new ArrayList<>();
+
     public List<List<String>> solveNQueens(int n) {
-        // code here
-        
-        List<List<String>> sc=new ArrayList<>();
-        
-        for(int i=0;i<n;i++){
-            List<String> row=new ArrayList<>();
-            for(int j=0;j<n;j++){
-                row.add("0");
-            }
-            sc.add(row);
+
+        char[][] board = new char[n][n];
+        for (char[] row : board) {
+            Arrays.fill(row, '.');
         }
-        boolean row[]=new boolean[n];
-        boolean dia[]=new boolean[2*n-1];
-        boolean antiDia[]=new boolean[2*n-1];
-        
-        List<String> temp=new ArrayList<>();
-        
-        solve(sc,temp,n,0,row,dia,antiDia);
+
+        boolean[] row = new boolean[n];
+        boolean[] dia = new boolean[2 * n - 1];
+        boolean[] antiDia = new boolean[2 * n - 1];
+
+        solve(n, 0, board, row, dia, antiDia);
         return ans;
-        
     }
-    
-    void solve(List<List<String>>sc,List<String> temp,int n,int j, boolean [] row , boolean []dia, boolean [] antiDia){
-        
-        if(j==n){
-            ans.add(new ArrayList<>(temp));
+
+    void solve(int n, int col, char[][] board,
+               boolean[] row, boolean[] dia, boolean[] antiDia) {
+
+        if (col == n) {
+            
+            List<String> temp = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                temp.add(new String(board[i]));
+            }
+            ans.add(temp);
             return;
         }
-        
-        for(int i=0;i<n;i++){
-            if(!row[i] && !dia[i+j] && !antiDia[i-j+n-1]){
-                sc.get(i).set(j,"1");
-                char [] r=new char[n];
-                Arrays.fill(r,'.');
-                r[i]='Q';
-                temp.add(new String(r));
+
+        for (int i = 0; i < n; i++) {
+            if (!row[i] && !dia[i + col] && !antiDia[i - col + n - 1]) {
+
                 
-                row[i]=true;
-                dia[i+j]=true;
-                antiDia[i-j+n-1]=true;
-                
-                solve(sc,temp,n,j+1,row,dia,antiDia);
-                
-                temp.remove(temp.size()-1);
-                sc.get(i).set(j,"0");
-                
-                row[i]=false;
-                dia[i+j]=false;
-                antiDia[i-j+n-1]=false;
+                board[i][col] = 'Q';
+                row[i] = true;
+                dia[i + col] = true;
+                antiDia[i - col + n - 1] = true;
+
+                solve(n, col + 1, board, row, dia, antiDia);
+
+               
+                board[i][col] = '.';
+                row[i] = false;
+                dia[i + col] = false;
+                antiDia[i - col + n - 1] = false;
             }
         }
-        
-        return;
-        
     }
 }
