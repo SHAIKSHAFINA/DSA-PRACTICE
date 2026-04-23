6073239851a8483ef1f4ed55871c5a0619b15c90
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-04-23 22:57:35 +0530

    Time: 213 ms (14.37%), Space: 46.6 MB (14.32%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,43 +1,44 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int n=board.length;
-        int m=board[0].length;
-        boolean ans=false;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int m=board.length;
+        int n=board[0].length;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(board[i][j]==word.charAt(0)){
-                     ans=backtrack(board,word,i,j,0);
-                    if(ans==true){
-                        return true;
-                    }
+                    if(solve(board,word,i,j,0)) return true;
                 }
             }
         }
-
-        return ans;
+        return false;
     }
 
-    boolean backtrack(char[][]board,String word,int i,int j,int k){
-        int n=board.length;
-        int m=board[0].length;
-        if(i<0 || j<0 || i>=n || j>=m || board[i][j]=='#'){
-            return false;
-        }
-         if(board[i][j]!=word.charAt(k)){
-            return false;
-        }
-        if(k==word.length()-1){
+    boolean solve(char[][] board, String word,int i,int j,int start){
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == '#') return false;
+        
+        if(board[i][j]!=word.charAt(start)) return false;
+        if(start==word.length()-1){
             return true;
         }
-        char original=board[i][j];
         
-            board[i][j]='#';
-           boolean found= backtrack(board,word,i+1,j,k+1) ||
-            backtrack(board,word,i-1,j,k+1) ||
-            backtrack(board,word,i,j+1,k+1) ||
-            backtrack(board,word,i,j-1,k+1);
-            board[i][j]=original;
-            return found;
-        
+        char temp=board[i][j];
+        board[i][j]='#';
+
+        int[] row={1,-1,0,0};
+        int[] col={0,0,1,-1};
+       
+
+        for(int k=0;k<4;k++){
+            int newRow=i+row[k];
+            int newCol=j+col[k];
+                
+                if(solve(board,word,newRow,newCol,start+1)){
+                    board[i][j]=temp;
+                    return true;
+                
+            }
+        }
+        board[i][j]=temp;
+        return false;
     }
 }
