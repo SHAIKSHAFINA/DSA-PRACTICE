f76491a9cc26d6178bd78aa0453d4cb4a633f365
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-04-23 17:27:52 +0530

    Time: 60 ms (95.1%), Space: 42.9 MB (54.93%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,59 +1,54 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        if(solve(board,0,0)) return;;
-    }
+        boolean row[][] =new boolean[9][9];
+        boolean col[][] =new boolean[9][9];
+        boolean box[][] =new boolean[9][9];
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] != '.') {
+                    int num = board[r][c] - '1';
+                    int boxIndex = (r / 3) * 3 + (c / 3);
 
-    boolean solve(char[][]b,int i,int j){
+                    row[r][num] = true;
+                    col[c][num] = true;
+                    box[boxIndex][num] = true;
+                }
+            }
+        }
+        if(solve(board,0,0,row,col,box)) return;
+}
+
+    boolean solve(char[][]b,int i,int j,boolean row[][],boolean col[][],boolean box[][]){
+
         if(i==9){
             return true;
         }
         if(j==9){
-            return solve(b,i+1,0);
+            return solve(b,i+1,0,row,col,box);
         }
 
         if(b[i][j]!='.'){
-            return solve(b,i,j+1);
+            return solve(b,i,j+1,row,col,box);
         }
         else{
             for(char x='1';x<='9';x++){
-                if(isCheck(i,j,b,x)){
+                int boxIndex=(i/3)*3 + (j/3);
+                int num=x-'1';
+                if(!row[i][num] && !col[j][num] && !box[boxIndex][num]){
                     b[i][j]=x;
-                    if(solve(b,i,j+1)) return true;
+                    row[i][num]=true;
+                    col[j][num]=true;
+                    box[boxIndex][num]=true;
+
+                    if(solve(b,i,j+1,row,col,box)) return true;
                     b[i][j]='.';
+
+                    row[i][num]=false;
+                    col[j][num]=false;
+                    box[boxIndex][num]=false;
                 }
             }
         }
         return false;
-    }
-
-    boolean isCheck(int i,int j,char[][]b,char x){
-        //row
-        for(int c=0;c<9;c++){
-            if(b[i][c]==x){
-                return false;
-            }
-        }
-
-        //col
-
-        for(int r=0;r<9;r++){
-            if(b[r][j]==x){
-                return false;
-            }
-        }
-
-        int row=(i/3)*3;
-        int col=(j/3)*3;
-
-        //3*3
-        for(int m=row;m<row+3;m++){
-            for(int n=col;n<col+3;n++){
-                if(b[m][n]==x){
-                    return false;
-                }
-            }
-        }
-
-        return true;
     }
 }
