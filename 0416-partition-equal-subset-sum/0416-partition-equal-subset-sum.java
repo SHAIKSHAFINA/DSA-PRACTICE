f90f34be66b544543906deee467f56c7321e8e3d
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-04-29 15:28:26 +0530

    Time: 38 ms (62.79%), Space: 43.9 MB (82.44%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -12,24 +12,21 @@ class Solution {
             return false;
         }
 
-        boolean dp[][]=new boolean[n+1][target+1];
+        boolean dp[]=new boolean[target+1];
 
-        for(int i=0;i<=n;i++){
-            dp[i][0]=true;
-        }
+        dp[0]=true;
+
+        
 
         for(int i=1;i<=n;i++){
-            for(int j=0;j<=target;j++){
+            for(int j=target;j>=0;j--){
                 if(nums[i-1]<=j){
-                    dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                    dp[j]=dp[j-nums[i-1]] || dp[j];
                 }
 
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
             }
         }
 
-        return dp[n][target];
+        return dp[target];
     }
 }
