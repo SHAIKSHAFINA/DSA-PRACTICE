83de0f55f6efad57db490a437197735facac8a20
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-04-29 15:23:04 +0530

    Time: 46 ms (58.05%), Space: 47.7 MB (46.56%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+
+        int target=sum/2;
+        if(sum % 2!=0){
+            return false;
+        }
+
+        boolean dp[][]=new boolean[n+1][target+1];
+
+        for(int i=0;i<=n;i++){
+            dp[i][0]=true;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=target;j++){
+                if(nums[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }
+
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][target];
+    }
+}
