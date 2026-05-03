42435ad1ccb31b73436f83e37a20b3cfc5b801fa
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-05-04 00:10:53 +0530

    Time: 0 ms (100%), Space: 42.6 MB (73.32%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
         int n=s.length();
-        if(s.length()!=goal.length()){
-            return false;
-        }
-        for(int i=0;i<n;i++){
-            if((s+s).contains(goal)){ // substring of s+s must be goal
-                return true;
-            }
-        }
-        return false;
+        int m=goal.length();
+
+        if(n!=m) return false;
+
+       String x=s+s;
+
+       if(x.contains(goal)){
+            return true;
+       }
+       return false;
     }
 }
