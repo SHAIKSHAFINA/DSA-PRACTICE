4591149cc4953bb3a7225cf73e7dfac0d7236e76
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-05-07 23:31:23 +0530

    Time: 7 ms (41.94%), Space: 199.2 MB (29.03%) - LeetHub

---
+++ 3660-jump-game-ix/3660-jump-game-ix.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int[] maxValue(int[] nums) {
+
+        int n = nums.length;
+
+        int[] prefixMax = new int[n];
+        int[] suffixMin = new int[n];
+
+        prefixMax[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
+        }
+
+        suffixMin[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = Math.min(suffixMin[i + 1], nums[i]);
+        }
+
+        int[] ans = new int[n];
+
+        int start = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            // component continues
+            if (prefixMax[i] > suffixMin[i + 1]) {
+                continue;
+            }
+
+            // finalize current component
+            int mx = Integer.MIN_VALUE;
+
+            for (int j = start; j <= i; j++) {
+                mx = Math.max(mx, nums[j]);
+            }
+
+            for (int j = start; j <= i; j++) {
+                ans[j] = mx;
+            }
+
+            start = i + 1;
+        }
+
+        // last component
+        int mx = Integer.MIN_VALUE;
+
+        for (int i = start; i < n; i++) {
+            mx = Math.max(mx, nums[i]);
+        }
+
+        for (int i = start; i < n; i++) {
+            ans[i] = mx;
+        }
+
+        return ans;
+    }
+}
