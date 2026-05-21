0c6bb109e66d43ee370c569316f25a30d0b0840c
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-05-21 20:02:19 +0530

    Time: 42 ms (84.2%), Space: 83.5 MB (41.19%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -1,28 +1,73 @@
-class Solution {
-    public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        int n=arr1.length;
+class TrieNode{
+        TrieNode child[];
+        boolean isEnd;
 
-        HashSet<Integer> s=new HashSet<>();
-        
-        for(int num:arr1){
-            while(num >0){
-                s.add(num);
-                num/=10;
+        TrieNode(){
+            child=new TrieNode[10];
+            isEnd=false;
+        }
+    }
+    
+    class Solution {
+        TrieNode root=new TrieNode();
+
+        // TrieNode getTrieNode(){
+        //     TrieNode node=new TrieNode();
+        //     // for(int i=0;i<10;i++){
+        //     //     node.child[i]=null;
+        //     // }
+
+        //     return node;
+        // }
+        public void insert(int num , TrieNode root){
+            TrieNode crawl=root;
+            String s=Integer.toString(num);
+
+            for(char ch:s.toCharArray()){
+                int idx=ch-'0';
+
+                if(crawl.child[idx]==null){
+                    crawl.child[idx]=new TrieNode();
+                }
+
+                crawl=crawl.child[idx];
+
             }
+
+            crawl.isEnd=true;
         }
 
-        int res=0;
-        for(int num:arr2){
-            while(!s.contains(num) && num >0){
-                num/=10;
+        int search(int num,TrieNode root){
+            TrieNode crawl=root;
+            String s=Integer.toString(num);
+            int length=0;
+
+            for(char ch:s.toCharArray()){
+                int idx=ch-'0';
+                if(crawl.child[idx]!=null){
+                    length++;
+                    crawl=crawl.child[idx];
+                }
+                else{
+                    break;
+                }
+
             }
 
-            if(num>0){
-                res=Math.max(res, (int)Math.log10(num)+1);
-            }
+            return length;
+        }
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        //TrieNode root=getTrieNode();
+
+        for(int num:arr1){
+            insert(num,root);
+        }
+        int res=0;
+
+        for(int num:arr2){
+            res=Math.max(res,search(num,root));
         }
 
         return res;
-        
     }
 }
