14e4cf9554342d3c6b1967d37ca069b54ca29561
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-11 21:13:59 +0530

    Time: 0 ms (100%), Space: 48 MB (56.21%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,10 +1,10 @@
 class Solution {
     public double myPow(double x, int n) {
         if(x==0) return 0;
-        if(n==1) return 1;
-        Long N=(long)(n);
+        if(n==0) return 1;
+        long N=n;
         
-       if(n<0){
+       if(N<0){
             x=1/x;
             N=-N;
        } 
