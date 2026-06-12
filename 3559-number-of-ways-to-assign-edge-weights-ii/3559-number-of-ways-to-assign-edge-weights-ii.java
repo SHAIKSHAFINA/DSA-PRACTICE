5c2ef80ef45ed104c3dcf9a84c9aadd04fd12cb6
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-12 22:09:08 +0530

    Time: 151 ms (47.83%), Space: 291 MB (86.96%) - LeetHub

---
+++ 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.java
@@ -0,0 +1,115 @@
+class Solution {
+
+    static final long MOD = 1000000007L;
+
+    int LOG;
+    int[][] up;
+    int[] depth;
+    long[] pow2;
+    List<Integer>[] graph;
+
+    public int[] assignEdgeWeights(int[][] edges, int[][] queries) {
+
+        int n = edges.length + 1;
+
+        graph = new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        LOG = 1;
+        while ((1 << LOG) <= n) LOG++;
+
+        up = new int[n + 1][LOG];
+        depth = new int[n + 1];
+
+        dfs(1, 1);
+
+        for (int j = 1; j < LOG; j++) {
+            for (int i = 1; i <= n; i++) {
+                up[i][j] = up[up[i][j - 1]][j - 1];
+            }
+        }
+
+        pow2 = new long[n + 1];
+        pow2[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            pow2[i] = (pow2[i - 1] * 2) % MOD;
+        }
+
+        int m = queries.length;
+        int[] ans = new int[m];
+
+        for (int i = 0; i < m; i++) {
+
+            int u = queries[i][0];
+            int v = queries[i][1];
+
+            int lca = lca(u, v);
+
+            int dist =
+                depth[u] + depth[v] - 2 * depth[lca];
+
+            if (dist == 0) {
+                ans[i] = 0;
+            } else {
+                ans[i] = (int) pow2[dist - 1];
+            }
+        }
+
+        return ans;
+    }
+
+    private void dfs(int node, int parent) {
+
+        up[node][0] = parent;
+
+        for (int nei : graph[node]) {
+
+            if (nei == parent) continue;
+
+            depth[nei] = depth[node] + 1;
+
+            dfs(nei, node);
+        }
+    }
+
+    private int lca(int u, int v) {
+
+        if (depth[u] < depth[v]) {
+            int temp = u;
+            u = v;
+            v = temp;
+        }
+
+        int diff = depth[u] - depth[v];
+
+        for (int j = LOG - 1; j >= 0; j--) {
+            if (((diff >> j) & 1) == 1) {
+                u = up[u][j];
+            }
+        }
+
+        if (u == v) return u;
+
+        for (int j = LOG - 1; j >= 0; j--) {
+
+            if (up[u][j] != up[v][j]) {
+                u = up[u][j];
+                v = up[v][j];
+            }
+        }
+
+        return up[u][0];
+    }
+}
