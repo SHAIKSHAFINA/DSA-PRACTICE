7c6bbf25eb2c6d0a053e95e3e5c727155f1ed2e5
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-14 12:46:57 +0530

    Time: 1 ms (59.24%), Space: 47.6 MB (80.31%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,7 +1,7 @@
 class Solution {
     public int trap(int[] arr) {
         int n=arr.length;
-        int max=0,lmax=0,rmax=n-1, ans=0;
+        int lmax=0,rmax=n-1,max=0,ans=0;
 
         for(int i=0;i<n;i++){
             if(arr[i]>arr[max]){
@@ -9,14 +9,14 @@ class Solution {
             }
         }
 
-        for(int i=1;i<max;i++){
+        for(int i=0;i<max;i++){
             if(arr[i]>arr[lmax]){
                 lmax=i;
             }
             ans+=arr[lmax]-arr[i];
         }
 
-        for(int i=n-2;i>max;i--){
+         for(int i=n-2;i>max;i--){
             if(arr[i]>arr[rmax]){
                 rmax=i;
             }
