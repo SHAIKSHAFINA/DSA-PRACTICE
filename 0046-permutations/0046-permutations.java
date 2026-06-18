3e130044d739524f0428c2789a0d53f7257402ae
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-18 16:10:11 +0530

    Time: 1 ms (87.71%), Space: 45.5 MB (63%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,34 +1,29 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        int n=nums.length;
         List<List<Integer>> l=new ArrayList<>();
+        List<Integer> sc=new ArrayList<>();
 
-        solve(nums,0,l);
+        boolean used[]=new boolean[n];
+        solve(nums,0,sc,l,used);
         return l;
     }
 
-    void solve(int nums[],int start,List<List<Integer>> res){
+    void solve(int nums[],int start, List<Integer> sc,List<List<Integer>> res,boolean used[]){
         int n=nums.length;
-        if(start==n){
-            List<Integer> sc=new ArrayList<>();
-            for(int x:nums){
-                sc.add(x);
-            }
-            res.add(sc);
+        if(sc.size()==n){
+            res.add(new ArrayList<>(sc));
             return;
         }
 
-        for(int i=start;i<n;i++){
-            swap(nums,start,i);
-            solve(nums,start+1,res);
-            swap(nums,start,i);
+        for(int i=0;i<n;i++){
+            if(used[i]) continue;
+            used[i]=true;
+            sc.add(nums[i]);
+            solve(nums,i+1,sc,res,used);
+            sc.remove(sc.size()-1);
+            used[i]=false;
         }
         return;
-
-    }
-
-    void swap(int nums[],int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
     }
 }
