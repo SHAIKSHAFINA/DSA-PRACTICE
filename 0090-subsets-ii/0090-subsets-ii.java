6fe760b0af704b71ab81480e5665f6d39a54c290
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-18 10:36:48 +0530

    Time: 3 ms (56.06%), Space: 45.1 MB (60.02%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -11,17 +11,17 @@ class Solution {
 
     void solve(int []nums,int start,List<List<Integer>> res,List<Integer> sc){
         int n=nums.length;
-        if(start==n){
-            res.add(new ArrayList<>(sc));
-            return;
-        }
-        sc.add(nums[start]);
-        solve(nums,start+1,res,sc);
-        sc.remove(sc.size()-1);
         
-        while(start+1 < n && nums[start]==nums[start+1]) start++;
+        res.add(new ArrayList<>(sc));
+        
+        for(int i=start;i<n;i++){
+            if(i> start && nums[i]==nums[i-1]) continue;
+            sc.add(nums[i]);
+            solve(nums,i+1,res,sc);
+            sc.remove(sc.size()-1);
+        }
 
-        solve(nums,start+1,res,sc);
+       // solve(nums,start+1,res,sc);
 
         return;
     }
