ecda5b5f3d8efb199b17aabc7364f689794d1ef2
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-18 13:32:31 +0530

    Time: 8 ms (72.89%), Space: 65.2 MB (73.13%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,30 +1,31 @@
 class Solution {
-    public List<List<String>> partition(String sb) {
-        List<List<String>> sc = new ArrayList<>();
-        List<String> l = new ArrayList<>();
-        solve(sb,l,sc,0);
-        return sc;
+    public List<List<String>> partition(String s) {
+        List<List<String>> res=new ArrayList<>();
+        List<String> sc=new ArrayList<>();
+        StringBuilder sb=new StringBuilder();
+
+        solve(s,sb,0,res,sc);
+        return res;
     }
 
-    void solve(String sb,List<String> l,List<List<String>> sc , int start){
-        if(start==sb.length()){
-            sc.add(new ArrayList<>(l));
+    void solve(String s,StringBuilder sb,int start,List<List<String>> res,List<String> sc){
+        int n=s.length();
+        if(start==n){
+            res.add(new ArrayList<>(sc));
             return;
         }
-        for(int i=start;i<sb.length();i++){
-            String t=sb.substring(start,i+1);
 
-            if(isPalindrome(sb,start,i)){
-                l.add(t);
-                solve(sb,l,sc,i+1);
-                l.remove(l.size()-1);
+        for(int i=start;i<n;i++){
+            if(isPalindrome(s,start,i)){
+                sc.add(s.substring(start,i+1));
+                solve(s,sb,i+1,res,sc);
+                sc.remove(sc.size()-1);
             }
         }
     }
 
     boolean isPalindrome(String sb,int l,int r){
-
-        while(l<r){
+        while(l<=r){
             if(sb.charAt(l)!=sb.charAt(r)){
                 return false;
             }
@@ -33,6 +34,5 @@ class Solution {
         }
 
         return true;
-        
     }
 }
