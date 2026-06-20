25cb093d15a51a632e27b0a225e737f7226a251f
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-20 10:38:19 +0530

    Time: 0 ms (100%), Space: 50.7 MB (99.97%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,22 +1,38 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n=nums.length;
-        HashSet<Integer> mp=new HashSet<>();
+        int l=0,r=n-1,ans=nums[0];
 
-        for(int num:nums){
-            if(mp.contains(num)){
-                mp.remove(num);
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            
+             if(mid%2==0){
+                if(nums[mid]==nums[mid-1]){
+                    r=mid-1;
+                }
+                else if(nums[mid]==nums[mid+1]){
+                    l=mid+1;
+                }
             }
             else{
-                mp.add(num);
+                if(nums[mid]==nums[mid-1]){
+                    l=mid+1;
+                }
+                else if(nums[mid]==nums[mid+1]){
+                     r=mid-1;
+                }
             }
-        }
 
-        int x=0;
-        for(int y:mp){
-            x=y;
-        }
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
 
-        return x;
+        }
+        return nums[l];
     }
 }
