9c9d9eefbf98cddb8162580a20e3707c5efdda10
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-20 10:23:42 +0530

    Time: 10 ms (4.66%), Space: 55.3 MB (8.4%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,38 +1,22 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n=nums.length;
-        int l=0,r=n-1,ans=nums[0];
+        HashSet<Integer> mp=new HashSet<>();
 
-        if(n==1) return nums[0];
-        if(nums[0]!=nums[1]) return nums[0];
-
-        if(nums[n-1]!=nums[n-2]) return nums[n-1];
-
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            
-             if(mid%2==0){
-                if(nums[mid]==nums[mid-1]){
-                    r=mid-1;
-                }
-                else if(nums[mid]==nums[mid+1]){
-                    l=mid+1;
-                }
+        for(int num:nums){
+            if(mp.contains(num)){
+                mp.remove(num);
             }
             else{
-                if(nums[mid]==nums[mid-1]){
-                    l=mid+1;
-                }
-                else if(nums[mid]==nums[mid+1]){
-                     r=mid-1;
-                }
+                mp.add(num);
             }
-
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-                return nums[mid];
-            }
-
         }
-        return nums[l];
+
+        int x=0;
+        for(int y:mp){
+            x=y;
+        }
+
+        return x;
     }
 }
