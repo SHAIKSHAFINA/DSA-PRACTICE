081f00e93741630d12c7f21542c515588cf20b4e
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-22 16:57:09 +0530

    Time: 11 ms (68.73%), Space: 50.1 MB (55.33%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int shipWithinDays(int[] arr, int k) {
+         int n=arr.length;
+        int max=arr[0];
+        int sum=0;
+        if(n<k) return -1;
+        
+        for(int i=0;i<n;i++){
+            max=Math.max(max,arr[i]);
+            sum+=arr[i];
+        }
+        
+        int l=max,r=sum;
+        
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            
+            if(solve(arr,k,mid)>k){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return l;
+        
+        
+    }
+    
+    int solve(int[] arr,int k,int mid){
+        int sum=0;
+        int c=1;
+        
+        for(int i=0;i<arr.length;i++){
+            if(sum+arr[i]<=mid){
+                sum+=arr[i];
+            }
+            else{
+                c++;
+                sum=arr[i];
+            }
+        }
+        
+        return c;
+    }
+}
