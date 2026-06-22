0efcb70bdbded22d7be5753acaf199eca46cc1f9
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-22 16:52:44 +0530

    Time: 1 ms (55.13%), Space: 42.9 MB (83.77%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,48 +1,46 @@
 class Solution {
     public int splitArray(int[] arr, int k) {
-        int n=arr.length;
-
-        if(k>n) return -1;
-
-        int s=0,max=Integer.MIN_VALUE;
-
-        for(int x:arr){
-            s+=x;
-            max=Math.max(max,x);
+         int n=arr.length;
+        int max=arr[0];
+        int sum=0;
+        if(n<k) return -1;
+        
+        for(int i=0;i<n;i++){
+            max=Math.max(max,arr[i]);
+            sum+=arr[i];
         }
-
-        int l=max,r=s;
-
+        
+        int l=max,r=sum;
+        
         while(l<=r){
-
             int mid=l+(r-l)/2;
-            if(possible(arr,mid,k)==true){
-                r=mid-1;
+            
+            if(solve(arr,k,mid)>k){
+                l=mid+1;
             }
             else{
-                l=mid+1;
+                r=mid-1;
             }
         }
         return l;
+        
+        
     }
-
-    boolean possible(int arr[],int dist,int k){
-        int n=arr.length;
-
-        int sum=0,c=1;
-
-        for(int i=0;i<n;i++){
-            if(arr[i]+sum>dist){
+    
+    int solve(int[] arr,int k,int mid){
+        int sum=0;
+        int c=1;
+        
+        for(int i=0;i<arr.length;i++){
+            if(sum+arr[i]<=mid){
+                sum+=arr[i];
+            }
+            else{
                 c++;
                 sum=arr[i];
             }
-            else{
-                sum+=arr[i];
-            }
-
-            if(c>k) return false;
         }
-
-        return true;
+        
+        return c;
     }
 }
