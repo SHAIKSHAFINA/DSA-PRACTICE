0ff417a0a61ae192f8d8e559521f1de67e04074a
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-22 09:05:22 +0530

    Time: 2 ms (46.57%), Space: 48.8 MB (51.42%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -4,38 +4,60 @@ class Solution {
         int m=nums2.length;
 
         int l=0,r=0;
-        int [] k=new int[n+m];
         int i=0;
+        int e1=-1;
+        int e2=-1;
+        int x=(n+m)/2;
 
         while(l<=n-1 && r<=m-1){
             if(nums1[l]<=nums2[r]){
-                k[i]=nums1[l];
+                if(i==x){
+                    e1=nums1[l];
+                }
+                if(i==(x-1)){
+                    e2=nums1[l];
+                }
                 l++;
             }
             else{
-                k[i]=nums2[r];
+                if(i==x){
+                    e1=nums2[r];
+                }
+                if(i==(x-1)){
+                    e2=nums2[r];
+                }
                 r++;
             }
             i++;
         }
 
         while(l<=n-1){
-            k[i]=nums1[l];
+           if(i==x){
+                e1=nums1[l];
+            }
+            if(i==(x-1)){
+                e2=nums1[l];
+            }
             l++;
             i++;
         }
 
         while(r<=m-1){
-            k[i]=nums2[r];
+            if(i==x){
+                e1=nums2[r];
+            }
+            if(i==(x-1)){
+                e2=nums2[r];
+            }
             r++;
             i++;
         }
 
         if((n+m)%2==0){
-            return (k[(n+m)/2]+k[((n+m)/2) -1]) / 2.0;
+            return (e1+e2)/ 2.0;
         }
         else{
-            return  k[(n+m)/2];
+            return  e1;
         }
     }
 }
