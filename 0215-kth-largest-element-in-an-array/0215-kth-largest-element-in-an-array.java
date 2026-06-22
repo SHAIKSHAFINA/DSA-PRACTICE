b0831c7e44fc6f4297d20e221bb3fcfac70e14b2
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-22 23:08:30 +0530

    Time: 82 ms (13.47%), Space: 75.7 MB (10.21%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -3,12 +3,14 @@ class Solution {
         int n=nums.length;
         PriorityQueue<Integer> pq=new PriorityQueue<>();
 
-        for(int i=0;i<n;i++){
-            pq.add(nums[i]);
-            if(pq.size()>k){
-                pq.poll();
-            }
+        for(int num:nums){
+            pq.add(num);
         }
+
+        while(!pq.isEmpty() && pq.size()>k){
+            pq.remove();
+        }
+
         return pq.peek();
     }
 }
