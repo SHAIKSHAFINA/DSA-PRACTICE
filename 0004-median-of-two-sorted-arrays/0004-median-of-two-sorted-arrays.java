948d2be363c90a400e777ab11b8c943e93cdf7fe
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-22 09:40:04 +0530

    Time: 1 ms (100%), Space: 49 MB (32.09%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,61 +3,36 @@ class Solution {
         int n=nums1.length;
         int m=nums2.length;
 
-        int l=0,r=0;
-        int i=0;
-        int e1=-1;
-        int e2=-1;
-        int x=(n+m)/2;
+        if(n>m) return findMedianSortedArrays(nums2,nums1);
 
-        while(l<=n-1 && r<=m-1){
-            if(nums1[l]<=nums2[r]){
-                if(i==x){
-                    e1=nums1[l];
+        int l=0,r=n;
+        int n1=(n+m+1)/2;
+
+        while(l<=r){
+            int p=l+(r-l)/2;
+            int q=n1-p;
+
+             int l1=(p==0)?Integer.MIN_VALUE :  nums1[p-1];
+             int l2=(q==0)?Integer.MIN_VALUE : nums2[q-1];
+             int r1=(p==n)?Integer.MAX_VALUE : nums1[p];
+             int r2=(q==m)?Integer.MAX_VALUE : nums2[q];
+
+            if(l1<=r2 && l2<=r1){
+                if((n+m)%2==0){
+                    return (Math.max(l1,l2)+Math.min(r1,r2)) / 2.0;
                 }
-                if(i==(x-1)){
-                    e2=nums1[l];
+                else{
+                    return Math.max(l1,l2);
                 }
-                l++;
+            }
+            else if(l1>r2){
+                r=p-1;
             }
             else{
-                if(i==x){
-                    e1=nums2[r];
-                }
-                if(i==(x-1)){
-                    e2=nums2[r];
-                }
-                r++;
+                l=p+1;
             }
-            i++;
         }
+        return -1;
 
-        while(l<=n-1){
-           if(i==x){
-                e1=nums1[l];
-            }
-            if(i==(x-1)){
-                e2=nums1[l];
-            }
-            l++;
-            i++;
-        }
-
-        while(r<=m-1){
-            if(i==x){
-                e1=nums2[r];
-            }
-            if(i==(x-1)){
-                e2=nums2[r];
-            }
-            r++;
-            i++;
-        }
-
-        if((n+m)%2==0){
-            return (e1+e2)/ 2.0;
-        }
-        else{
-            return  e1;
-        }
     }
 }
