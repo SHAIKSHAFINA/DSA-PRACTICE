bb6e3e5d82d946b7e93813915669177ab2143228
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-22 08:52:40 +0530

    Time: 2 ms (46.57%), Space: 49 MB (15.21%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,45 +1,41 @@
 class Solution {
-    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
-        int n1=arr1.length;
-        int n2=arr2.length;
-        if(n1>n2) return findMedianSortedArrays(arr2,arr1);
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        int m=nums2.length;
 
-        int low=0,high=n1;
-        int left=(n1+n2+1)/2;
-        int n=n1+n2;
-        
-        while(low<=high){
-            int mid1=low+(high-low)/2;
-            int mid2=left-mid1;
+        int l=0,r=0;
+        int [] k=new int[n+m];
+        int i=0;
 
-
-
-            int l1= Integer.MIN_VALUE;
-            int l2= Integer.MIN_VALUE;
-            int r1= Integer.MAX_VALUE;
-            int r2= Integer.MAX_VALUE;
-           
-           if(mid1<n1) r1=arr1[mid1];
-           if(mid2<n2) r2=arr2[mid2];
-
-           if(mid1-1>=0) l1=arr1[mid1-1];
-           if(mid2-1>=0) l2=arr2[mid2-1];
-
-            if(l1<=r2 && l2<=r1){
-                if(n%2==1) return Math.max(l1,l2);
-                return (double)(Math.max(l1,l2) + Math.min(r1,r2)) / 2.0;
+        while(l<=n-1 && r<=m-1){
+            if(nums1[l]<=nums2[r]){
+                k[i]=nums1[l];
+                l++;
             }
-
-            else if(l1>r2){
-                high=mid1-1;
-            }
-
             else{
-                low=mid1+1;
+                k[i]=nums2[r];
+                r++;
             }
-            
+            i++;
         }
 
-        return 0;
+        while(l<=n-1){
+            k[i]=nums1[l];
+            l++;
+            i++;
+        }
+
+        while(r<=m-1){
+            k[i]=nums2[r];
+            r++;
+            i++;
+        }
+
+        if((n+m)%2==0){
+            return (k[(n+m)/2]+k[((n+m)/2) -1]) / 2.0;
+        }
+        else{
+            return  k[(n+m)/2];
+        }
     }
 }
