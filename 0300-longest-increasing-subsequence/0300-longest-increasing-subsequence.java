e7602794f0374e0fcba7db42194e2b591357aefd
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-24 10:36:59 +0530

    Time: 464 ms (5.01%), Space: 237.5 MB (7.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,22 +1,24 @@
 class Solution {
-    public int lengthOfLIS(int[] arr) {
-        int n=arr.length;
-        int dp[]=new int[n];
-        Arrays.fill(dp,1);
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        int dp[][]=new int[n][n+1];
 
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(arr[j]<arr[i]){
-                    dp[i]=Math.max(dp[i],1+dp[j]);
-                }
-            }
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
         }
-         int max=Integer.MIN_VALUE;
 
-         for(int x:dp){
-             max=Math.max(max,x);
-         }
+        return solve(nums,0,-1,n,dp);
+    }
 
-        return max;
+    int solve(int[]nums,int i,int prev,int n,int dp[][]){
+        
+        if(i==n) return 0;
+        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+        dp[i][prev+1]=solve(nums,i+1,prev,n,dp);//not take
+
+        if(prev==-1 || nums[i] > nums[prev]){
+            dp[i][prev+1]= Math.max(dp[i][prev+1],1+solve(nums,i+1,i,n,dp));
+        }
+        return dp[i][prev+1];
     }
 }
