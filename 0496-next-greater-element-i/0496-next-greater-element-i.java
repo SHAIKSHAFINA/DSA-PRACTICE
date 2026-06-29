f9d0b42e133de2f6483e5c46a3cea11041e6de82
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-29 12:16:30 +0530

    Time: 4 ms (74.78%), Space: 45.3 MB (49.64%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,23 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
+        int n=nums1.length;
+        int m=nums2.length;
 
-        HashMap<Integer, Integer> nge = new HashMap<>();
-        Deque<Integer> st = new ArrayDeque<>();
+        int ans[]=new int[n];
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        Stack<Integer> st=new Stack<>();
 
-        for (int x : nums2) {
-            while (!st.isEmpty() && st.peek() < x) {
-                nge.put(st.pop(), x);
+        for(int x:nums2){
+            while(!st.isEmpty() && st.peek() < x){
+                mp.put(st.pop(),x);
             }
             st.push(x);
         }
 
-        while (!st.isEmpty()) {
-            nge.put(st.pop(), -1);
+        while(!st.isEmpty()){
+            mp.put(st.pop(),-1);;
         }
 
-        for (int i = 0; i < nums1.length; i++) {
-            ans[i] = nge.get(nums1[i]); 
+        for(int i=0;i<n;i++){
+            ans[i]=mp.get(nums1[i]);
         }
 
         return ans;
