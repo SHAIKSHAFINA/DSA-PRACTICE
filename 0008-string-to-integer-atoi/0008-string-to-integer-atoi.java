57d741ee5dfa6e9ce6d14fa8dae716bb3e97c1bc
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-30 12:13:27 +0530

    Time: 1 ms (100%), Space: 44 MB (25.02%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,27 +1,27 @@
 class Solution {
     public int myAtoi(String s) {
         int n=s.length();
-        int num=0 , sign=+1;
-        int i=0;
+        int num=0,sign=+1,i=0;
 
-       while( i<n && s.charAt(i)==' '){
+        while(i<n && s.charAt(i)==' '){
             i++;
         }
+
         if(i<n && (s.charAt(i)=='+' || s.charAt(i)=='-')){
             sign=(s.charAt(i)=='-')?-1:1;
             i++;
         }
-            
+
         while(i<n && isDigit(i,s)){
-            int digit=s.charAt(i)-'0';
-                if(num >(Integer.MAX_VALUE - digit)/10){
-                    return sign == 1 ?Integer.MAX_VALUE :Integer.MIN_VALUE;
-                }
-                num=num*10+(s.charAt(i)-'0');
+            int d=s.charAt(i)-'0';
+            if(num > (Integer.MAX_VALUE-d)/10){
+                return sign==1?Integer.MAX_VALUE:Integer.MIN_VALUE;
+            }
+            num=num*10+d;
             i++;
         }
 
-        return sign *num;
+        return sign * num;
     }
 
     boolean isDigit(int i,String s){
