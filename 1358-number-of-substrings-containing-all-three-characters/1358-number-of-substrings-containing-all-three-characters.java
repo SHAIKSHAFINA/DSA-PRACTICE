8a4f98dc13b41cfd5283e8d59d63072c8256655e
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-06-30 20:33:57 +0530

    Time: 15 ms (52.53%), Space: 46.4 MB (30.71%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -4,18 +4,18 @@ class Solution {
         int c=0;
         int hash[]=new int[3];
         int l=0,r=0;
-        
 
         while(r<n){
             hash[s.charAt(r)-'a']++;
             while(hash[0]>0 && hash[1]>0 && hash[2]>0){
-                    c+=n-r;
-                    hash[s.charAt(l)-'a']--;
-                    l++;
-                
+                c+=n-r;
+                hash[s.charAt(l)-'a']--;
+                l++;
             }
+            
             r++;
         }
+
         return c;
     }
 }
