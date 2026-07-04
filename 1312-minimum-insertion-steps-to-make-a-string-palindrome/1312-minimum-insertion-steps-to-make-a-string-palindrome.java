ec169e4bdaf0b9aee9f530f7a4e3c827c23383cc
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-07-04 14:22:28 +0530

    Time: 19 ms (85.55%), Space: 47.2 MB (63.09%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,23 +1,24 @@
 class Solution {
-    public int minInsertions(String s1) {
-        StringBuilder sb=new StringBuilder(s1);
-        String s2=sb.reverse().toString();
-        int n=s1.length();
-        int m=s2.length();
-        int dp[][]=new int[n+1][m+1];
+    public int minInsertions(String s) {
+        int n=s.length();
+        int dp[][]=new int[n+1][n+1];
         
-        dp[0][0]=0;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+        for(int i=0;i<=n;i++){
+            Arrays.fill(dp[i],-1);
         }
-        
-        return n-dp[n][m];
+        return solve(s,0,n-1,dp);
+    }
+
+    int solve(String s,int i,int j,int dp[][]){
+        if(i>=j){
+            return 0;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s.charAt(i)==s.charAt(j)){
+            return dp[i][j]=solve(s,i+1,j-1,dp);
+        }
+        else{
+            return dp[i][j]=Math.min(1+solve(s,i+1,j,dp),1+solve(s,i,j-1,dp));
+        }
     }
 }
