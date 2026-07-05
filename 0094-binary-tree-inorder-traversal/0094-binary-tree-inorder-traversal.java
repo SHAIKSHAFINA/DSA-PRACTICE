b80e1867f0d9f6cf1560eb08261304bd6c024ef1
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-07-05 18:37:05 +0530

    Time: 0 ms (100%), Space: 43.4 MB (14.89%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,43 +1,33 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> sc=new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
 
-        if(root==null) return sc;
-        TreeNode curr=root;
+        TreeNode curr = root;
 
-        while(curr!=null){
-            if(curr.left==null){
-                sc.add(curr.val);
-                curr=curr.right;
-            }
-            else{
-                TreeNode l=curr.left;
-                while(l.right!=null){
-                    l=l.right;
+        while (curr != null) {
+
+            if (curr.left == null) {
+                ans.add(curr.val);
+                curr = curr.right;
+            } else {
+
+                TreeNode pre = curr.left;
+
+                while (pre.right != null && pre.right != curr) {
+                    pre = pre.right;
                 }
-                l.right=curr;
 
-                TreeNode temp=curr;
-                curr=curr.left;
-                temp.left=null;
+                if (pre.right == null) {
+                    pre.right = curr;
+                    curr = curr.left;
+                } else {
+                    pre.right = null;
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
             }
         }
 
-        return sc;
+        return ans;
     }
 }
