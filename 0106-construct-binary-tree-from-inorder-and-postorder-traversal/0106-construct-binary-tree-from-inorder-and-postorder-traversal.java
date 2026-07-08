3c4c31ae3bf5d3924152f021a04812981b274f40
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-07-08 15:21:11 +0530

    Time: 2 ms (64.82%), Space: 46.3 MB (49.44%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,24 +15,25 @@
  */
 class Solution {
     int postIdx = 0;
+    Map<Integer,Integer> mp=new HashMap<>();
+
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         int x=inorder.length;
         postIdx = postorder.length - 1;
+
+        for(int i=0;i<x;i++){
+            mp.put(inorder[i],i);
+        }
         return order(inorder,postorder,0,x-1);
     }
     TreeNode order(int[] in,int[]post,int s,int e){
-        int j=0;
+
         if(s>e){
             return null;
         }
-        for(int i=s;i<=e;i++){
-            if(post[postIdx]==in[i]){
-                j=i;
-                break;
-            }
-        }
-
-        TreeNode root=new TreeNode(post[postIdx--]);
+        int NodeVal=post[postIdx--];
+        TreeNode root=new TreeNode(NodeVal);
+        int j=mp.get(NodeVal);
 
         root.right=order(in,post,j+1,e);
         root.left=order(in,post,s,j-1);
