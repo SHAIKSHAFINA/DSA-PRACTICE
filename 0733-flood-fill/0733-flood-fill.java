fc007dfb2ab29677672dbfc6e58a72628a35b93b
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-07-15 11:01:05 +0530

    Time: 0 ms (100%), Space: 46.5 MB (90.96%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -3,6 +3,7 @@ class Solution {
         int n=image.length;
         int m=image[0].length;
          int temp=image[sr][sc];
+         if(temp==color) return image;
 
         solve(image,sr,sc,color,n,m,temp);
         return image;
@@ -10,17 +11,16 @@ class Solution {
 
     void solve(int[][]image,int i,int j,int color,int n,int m,int temp){
         if(i<0 || j<0 || i>=n || j>=m) return;
-        int old=temp;
-        if(old==color) return;
+        if(temp==color) return;
 
         if(image[i][j]!=temp){ return;}
-        if(image[i][j]==temp){
-            image[i][j]=color;
-        }
-        solve(image,i-1,j,color,n,m,temp);
-        solve(image,i,j-1,color,n,m,temp);
-        solve(image,i+1,j,color,n,m,temp);
-        solve(image,i,j+1,color,n,m,temp);
+        image[i][j]=color;
 
+        int dr[]={-1,1,0,0};
+        int dc[]={0,0,-1,1};
+
+        for(int k=0;k<4;k++){
+            solve(image,i+dr[k],j+dc[k],color,n,m,temp);
+        }
     }
 }
