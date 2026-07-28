4a9ecb0bdc1910aa8e8899976cd2bc0d7e992c55
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-07-28 17:09:09 +0530

    Time: 0 ms (100%), Space: 44 MB (27.85%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,30 +1,41 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n=nums.length;
-        int l=0,r=n-1;
+        int idx=n-1;
+
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]>nums[i+1]){
+                idx=i;
+                break;
+            }
+        }
+
+        int x=binary(0,idx,nums,target);
+        int y=binary(idx+1,n-1,nums,target);
+
+        if(x!=-1){
+            return x;
+        }
+        else if(y!=-1){
+            return y;
+        }
+        return -1;
+
+    }
+
+    int binary(int l,int r,int[] nums,int target){
+        int n=nums.length;
 
         while(l<=r){
             int mid=l+(r-l)/2;
             if(nums[mid]==target){
                 return mid;
             }
-
-            else if(nums[l]<=nums[mid]){
-                if(nums[l]<=target&& target<nums[mid]){
-                    r=mid-1;
-                }
-                else{
-                    l=mid+1;
-                }
+            else if(nums[mid]>target){
+                r--;
             }
             else{
-                if(target >nums[mid] && target<=nums[r]){
-                     l=mid+1;
-                   
-                }
-                else{
-                     r=mid-1;
-                }
+                l++;
             }
         }
         return -1;
