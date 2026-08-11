b85afde8cec836a303ff77e5c73c36625a668101
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-08-11 22:35:28 +0530

    Time: 5 ms (28.22%), Space: 54.8 MB (5.22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,34 +1,41 @@
 class Solution {
+    int c=0;
     public int numIslands(char[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int c=0;
+
+        boolean [][] vis=new boolean[n][m];
+        ArrayList<Integer> sc=new ArrayList<>();
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
+                if(grid[i][j]=='1' && !vis[i][j]){
                     c++;
-                    dfs(grid,i,j,n,m);
+                    dfs(i,j,grid,m,n,vis);
                 }
             }
         }
         return c;
     }
 
-    void dfs(char[][]grid,int i,int j,int n,int m){
-        if(i<0 || i>=n || j<0 || j>=m){
-            return;
-        }
+    void dfs(int i,int j,char[][] grid,int m,int n,boolean [][] vis){
+        
+        vis[i][j]=true;
 
-        if(grid[i][j]=='0') return;
-
-        grid[i][j]='0';
-
-        int dr[]={1,-1,0,0};
-        int dc[]={0,0,1,-1};
+        int dr[]={-1,1,0,0};
+        int dc[]={0,0,-1,1};
 
         for(int k=0;k<4;k++){
-            dfs(grid,i+dr[k],j+dc[k],n,m);
+            int nr=i+dr[k];
+            int nc=j+dc[k];
+
+            if(nr<0 || nr>=n) continue; 
+            if(nc<0 || nc>=m) continue;
+
+            if(grid[nr][nc]=='1' && !vis[nr][nc]){
+                dfs(nr,nc,grid,m,n,vis);
+            }
         }
+
     }
 }
