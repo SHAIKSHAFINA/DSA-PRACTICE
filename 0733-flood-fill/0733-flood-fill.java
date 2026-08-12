7d5b54d55cc4ba752f254b3118740d4ad29dae2f
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-08-12 07:54:51 +0530

    Time: 1 ms (38.6%), Space: 46.1 MB (99.34%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,26 +1,52 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n=image.length;
-        int m=image[0].length;
-         int temp=image[sr][sc];
-         if(temp==color) return image;
+        int m=image.length;
+        int n=image[0].length;
+        boolean vis[][]=new boolean[m][n];
+        int a[][]=new int[m][n];
 
-        solve(image,sr,sc,color,n,m,temp);
-        return image;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==sr && j==sc && !vis[i][j]){
+                    a[i][j]=color;
+                    vis[i][j]=true;
+                    dfs(i,j,vis,a,image,sr,sc,m,n);
+                }
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!vis[i][j]){
+                    a[i][j]=image[i][j];
+                }
+            }
+        }
+
+        return a;
     }
 
-    void solve(int[][]image,int i,int j,int color,int n,int m,int temp){
-        if(i<0 || j<0 || i>=n || j>=m) return;
-        if(temp==color) return;
-
-        if(image[i][j]!=temp){ return;}
-        image[i][j]=color;
+    void dfs(int i,int j,boolean [][] vis,int a[][],int image[][],int sr,int sc,int m,int n){
+        if(i<0 || i>=m || j<0 || j>=n){
+            return;
+        }
 
         int dr[]={-1,1,0,0};
         int dc[]={0,0,-1,1};
 
         for(int k=0;k<4;k++){
-            solve(image,i+dr[k],j+dc[k],color,n,m,temp);
+            int nr=i+dr[k];
+            int nc=j+dc[k];
+
+            if(nr<0 || nr>=m) continue;
+            if(nc<0 || nc>=n) continue;
+
+            if(!vis[nr][nc] && image[nr][nc]==image[sr][sc]){
+                a[nr][nc]=a[sr][sc];
+                vis[nr][nc]=true;
+                dfs(nr,nc,vis,a,image,sr,sc,m,n);
+            }
         }
+
     }
 }
