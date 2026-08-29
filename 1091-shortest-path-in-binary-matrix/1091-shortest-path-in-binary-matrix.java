bbcd5985d9ffa4949cf7d92640c1648d4ffb7ec2
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-08-29 11:49:20 +0530

    Time: 16 ms (55.2%), Space: 47.6 MB (59.44%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -0,0 +1,50 @@
+class Solution {
+    class Pair{
+        int i;
+        int j;
+        Pair(int x,int y){
+            i=x;
+            j=y;
+        }
+    }
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n=grid.length;
+        if(grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
+
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(0,0));
+
+        int dr[]={-1, -1, -1, 0, 1, 1, 1, 0};
+        int dc[]={-1, 0, 1, 1, 1, 0, -1, -1};
+        boolean vis[][]=new boolean[n][n];
+        vis[0][0]=true;
+        int[][] ar=new int[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(ar[i],Integer.MAX_VALUE);
+        }
+
+        ar[0][0]=1;
+        
+
+        while(!q.isEmpty()){
+            Pair p=q.poll();
+            int a=p.i;
+            int b=p.j;
+
+            for(int k=0;k<8;k++){
+                int r=a+dr[k];
+                int c=b+dc[k];
+
+                if(r<0 || c<0 || r>=n || c>=n) continue;
+                if(!vis[r][c] && grid[r][c]==0){
+                    q.add(new Pair(r,c));
+                    vis[r][c]=true;
+                    ar[r][c]=Math.min(ar[r][c],ar[a][b]+1);
+                }
+            }
+        }
+
+        return ar[n-1][n-1]==Integer.MAX_VALUE?-1:ar[n-1][n-1];
+
+    }
+}
