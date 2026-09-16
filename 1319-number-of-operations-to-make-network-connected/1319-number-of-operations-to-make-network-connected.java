30ae597be8236d6c1f6691c060c29c2e47c25471
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-09-16 08:43:21 +0530

    Time: 8 ms (45.89%), Space: 65.4 MB (88.74%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,66 @@
+class dsu{
+    int parent[];
+    int rank[];
+    int size[];
+
+    dsu(int n){
+        parent=new int[n+1];
+        rank=new int[n+1];
+        size=new int[n+1];
+
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+            rank[i]=0;
+            size[i]=1;
+        }
+    }
+
+    int findParent(int node){
+        if(node==parent[node]) return node;
+        return parent[node]=findParent(parent[node]);
+    }
+
+    void unionBySize(int u,int v){
+        int pu=findParent(u);
+        int pv=findParent(v);
+
+        if(pu==pv) return;
+
+        if(size[pu]<size[pv]){
+            parent[pu]=pv;
+            size[pv]+=size[pu];
+        }
+        else{
+            parent[pv]=pu;
+            size[pu]+=size[pv];
+        }
+    }
+}
+
+
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        dsu ds=new dsu(n);
+        int c=0;
+        int total=n;
+        
+        for(int e[]:connections){
+            int u=e[0];
+            int v=e[1];
+
+            if(ds.findParent(u)!=ds.findParent(v)){
+                total--;
+                ds.unionBySize(u,v);
+            }
+            else if(ds.findParent(u)==ds.findParent(v)){
+                c++;
+            }
+            
+        }
+
+        int req=total-1;
+        
+        if(c>=req) return req;
+        else return -1;
+    }
+}
