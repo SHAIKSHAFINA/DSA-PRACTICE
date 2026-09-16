cd84ef2c00642dd7e12ca9eda99ebbd392d02fcd
Shaik Shafina <153089644+SHAIKSHAFINA@users.noreply.github.com>
2026-09-16 08:46:21 +0530

    Time: 8 ms (45.89%), Space: 65.4 MB (88.74%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -8,7 +8,7 @@ class dsu{
         rank=new int[n+1];
         size=new int[n+1];
 
-        for(int i=1;i<=n;i++){
+        for(int i=0;i<n;i++){
             parent[i]=i;
             rank[i]=0;
             size[i]=1;
